b3c3dbe0f2784eb5bcafd63390385a2089f7d012
wiston-pastor <77182864+wiston-pastor@users.noreply.github.com>
2024-09-09 08:16:21 +0000

    first change before sleep day 1

--- practicaReact/src/App.jsx
+++ practicaReact/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Square({value,onSquareClick}){
+  return(
+    <button 
+      className='square' 
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board(){
+  const [square,setSquare]=useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquare = square.slice();
+    nextSquare[i]= "X";
+    setSquare(nextSquare);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='board-row'>
+      <Square value={square[0]} onSquareClick={()=>handleClick(0)}/>
+      <Square value={square[1]} onSquareClick={()=>handleClick(1)}/>
+      <Square value={square[2]} onSquareClick={()=>handleClick(2)}/>     
+    </div>
+
+    <div className='board-row'>
+      <Square value={square[3]} onSquareClick={()=>handleClick(3)}/>
+      <Square value={square[4]} onSquareClick={()=>handleClick(4)}/>
+      <Square value={square[5]} onSquareClick={()=>handleClick(5)}/> 
+    </div>
+
+    <div className='board-row'>
+      <Square value={square[6]} onSquareClick={()=>handleClick(6)}/>
+      <Square value={square[7]} onSquareClick={()=>handleClick(7)}/>
+      <Square value={square[8]} onSquareClick={()=>handleClick(8)}/>  
+    </div>
+    
     </>
   )
 }
-
-export default App
